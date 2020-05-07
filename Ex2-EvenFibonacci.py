277e4feee1c5d8402163f0f4f624d40bff7d7277
Camila Pessoa <caape13@hotmail.com>
2020-05-07 20:23:57 +0100

    First commit

---
+++ Ex2-EvenFibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(maximo):
+    ''' (int) -> list
+
+        Return the Fibonacci sequence with the maximum value given by maximo.
+
+        fibonacci(89) -> [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        '''
+def sum_even(x):
+    ''' (list) -> int
+
+    Return the sum of even numbers in a list
+
+    sum_even([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]) -> 44
+    '''
+    even
