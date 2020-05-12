558600b0b59e2c5cec9a2e846bcd080f53a43370
Camila Pessoa <caape13@hotmail.com>
2020-05-12 18:03:48 +0100

    Define function

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+#Problem 4: Find the largest palindrome made from the product of two 3-digit numbers.
+
+def palindrome():
+    ''' (int) -> int
+    
+    return the largest palindrome from the product of the numbers with
+    the given number of digits
+
+    palindrome(2) -> 9009 '''
