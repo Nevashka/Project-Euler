ccf1672c802e365738df6608977663f3ae88c3fe
Camila Pessoa <caape13@hotmail.com>
2020-05-12 18:52:04 +0100

    Fix function bug

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
 #Problem 4: Find the largest palindrome made from the product of two 3-digit numbers.
 
-def palindrome(digits):
+def largest(digits):
     ''' (int) -> int
 
     return the largest palindrome from the product of the numbers with
@@ -8,15 +8,15 @@ def palindrome(digits):
 
     palindrome(2) -> 9009 '''
 
-
     first = 10**(digits-1)
     last = (10**digits)-1
 
-    while True:
-        for i in range(last, first, -1):
-            for x in range(last, first, -1):
-                product = i * x
-                if str(product) == str(product)[::-1]:
-                    return product
-                else:
-                    product = 0
+    palindrome = [0]
+
+    for i in range(last, first, -1):
+        for x in range(last, first, -1):
+            product = i * x
+            if str(product) == str(product)[::-1] and palindrome[0] < product:
+                palindrome[0] = product
+
+    return palindrome[0]
