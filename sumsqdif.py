78b4ccd19944e0b12343d7d4e86f23a7ff58742f
Camila Pessoa <caape13@hotmail.com>
2020-05-21 08:57:21 +0100

    Fix for sentence

--- sumsqdif.py
+++ sumsqdif.py
@@ -8,13 +8,14 @@ def sum_square_dif (x,y):
     (x,y) and the square of the sum.
 
     sum_square_dif (1,10) -> 2640
+    sum_square_dif (1,100) -> 25164150
     '''
 
     sum_squares = 0
     square_sum = 0
 
-    for i in range(x,y)
-        sum_squares = sum_squares + i**2
+    for i in range(x,y+1):
+        sum_squares = sum_squares + (i**2)
         square_sum = square_sum + i
 
     return (square_sum**2)-sum_squares
