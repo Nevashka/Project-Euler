7fddb7943477592249adabd9ae2b95a76d67a001
Camila Pessoa <caape13@hotmail.com>
2020-05-21 08:09:46 +0100

    Define smallest function

---
+++ smultiple.py
@@ -0,0 +1,10 @@
+#Problem 5: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def smallest_multiple(x,y):
+    '''(int), (int) -> int
+
+    Return the smallest number that can be divised by each numbers from x to y
+    without any remainder.
+
+    smallest_multiple(1,10) -> 2520
+    '''
