91f8682c2380ecf82b224683cc35fb89752c177f
Camila Pessoa <caape13@hotmail.com>
2020-05-21 08:39:21 +0100

    Define function

---
+++ sum-sq-dif.py
@@ -0,0 +1,11 @@
+#Problem 6: Find the difference between the sum of the squares of the first one
+#hundred natural numbers and the square of the sum.
+
+def sum_square_dif (x,y):
+    '''(int), (int) -> int
+
+    Return the difference between the sum of the squares of two natural numbers
+    (x,y) and the square of the sum.
+
+    sum_square_dif (1,10) -> 2640
+    '''
