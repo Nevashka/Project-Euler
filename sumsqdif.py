83f19b2e47af5905232851583039fdac76999c11
Camila Pessoa <caape13@hotmail.com>
2020-05-21 08:50:34 +0100

    Change file name

--- sumsqdif.py
+++ sumsqdif.py
@@ -9,3 +9,12 @@ def sum_square_dif (x,y):
 
     sum_square_dif (1,10) -> 2640
     '''
+
+    sum_squares = 0
+    square_sum = 0
+
+    for i in range(x,y)
+        sum_squares = sum_squares + i**2
+        square_sum = square_sum + i
+
+    return (square_sum**2)-sum_squares
